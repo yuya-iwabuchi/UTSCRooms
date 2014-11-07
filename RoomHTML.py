ca7e7553c728366cfe31f3b3726d81dbced35e0b
Yuya <yuya.iwabuchi@gmail.com>
2014-11-07 11:38:39 -0500

    Update print and algos

--- RoomHTML.py
+++ RoomHTML.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import datetime
+import json
 
 
 import timeit
@@ -9,36 +10,28 @@ start = timeit.default_timer()
 
 
 URL = 'https://www.utsc.utoronto.ca/~registrar/scheduling/room_schd'
+HTML = requests.get(URL).text
+WEEK_PICKER = 'bgcolor="#FFF0F1"'   # Pivot
+# This would contain the current week with day with the above color
+WEEK_PICKER_RE = '<tr>.*?' + WEEK_PICKER + '.*?</tr>'
 
 
 def collect():
-    HTML = requests.get(URL).text
 
     # stop = timeit.default_timer()
     # print "Initial Download: %f seconds" % (stop - start)
     # start = timeit.default_timer()
 
-    WEEK_PICKER = 'bgcolor="#FFF0F1"'   # Pivot
-
-    WEEK_PICKER_RE = '<tr>.*?' + WEEK_PICKER + '.*?</tr>'
-    # This would contain the current week with day with the above color
-
     week_html = re.search(WEEK_PICKER_RE, HTML).group(0)
     week_days = []
     for d in re.findall('>[0-9]+<', week_html):
         week_days.append(d[1:-1])
-    date = re.search(WEEK_PICKER + '.*?<', week_html).group(0)[len(WEEK_PICKER)+1: -1]
+    date = re.search(WEEK_PICKER + '.*?<', week_html).group(0)[len(WEEK_PICKER) + 1: -1]
     day = week_days.index(date)
     post_week = re.search('value=".*?"', week_html).group(0)[7:-1]
 
-    current_time = datetime.datetime.now().strftime('%H:%M').split(':')
-    # current_time = ['14', '00']
-    # print current_time
-    current_block_time = [current_time[0], '{0:02}'.format(int(current_time[1])//30 * 30)]
-
-    print 'Date: %s\nPost Week: %s\nDay Index: %s\nTime: %s:%s\nBlock Time: %s:%s\n' % \
-          (date, post_week.split(':')[0], day,
-           current_time[0], current_time[1], current_block_time[0], current_block_time[1])
+    # print 'Date of Month: %s\nPost Week: %s\nWeek Day Index: %s\n' % \
+    #       (date, post_week, day)
 
     post_rooms = []
 
@@ -84,7 +77,7 @@ def collect():
     #
     # f.close()
 
-    room_data = {}
+    room_data = {'collect': [datetime.datetime.now().strftime('%b %d, %Y'), date, day, post_week]}
 
     for room in re.findall('<h5>.*?<b>University_of_Toronto</b>', post_html, re.DOTALL):
         name = re.search('<h5>.*?</h5>', room).group(0)[4:-5]
@@ -101,7 +94,8 @@ def collect():
             'storage': re.search('<td>Storage: .*?</td>', room).group(0)[13:-5],
             'wheelchair': re.search('<td>Wheelchair: .*?</td>', room).group(0)[16:-5],
             'window': re.search('<td>Window: .*?</td>', room).group(0)[12:-5],
-            'FM Hearing Assist Transmitter': re.search('<td>FM Hearing Assist Transmitter: .*?</td>', room).group(0)[35:-5]}
+            'FM Hearing Assist Transmitter': re.search('<td>FM Hearing Assist Transmitter: .*?</td>',
+                                                       room).group(0)[35:-5]}
 
         room_data[name]['images'] = []
         for img in re.findall('<img src=".*?"', room):
@@ -109,22 +103,27 @@ def collect():
 
         room_data[name]['times'] = []
         time_html = re.search('<!-- START ROW OUTPUT -->.*?<!-- END ROW OUTPUT -->', room, re.DOTALL).group(0)
-        for time in re.findall("<center>[^=]*?colspan='[1-9][0-9]*' rowspan='[1-9][0-9]*'", time_html):
-            t = re.search("<center>.*?</center>", time).group(0)[8:-9].split(':')
-            for block in range(int(re.search("rowspan='[1-9][0-9]*'", time).group(0)[9:-1])):
+        for ti in re.findall("<center>[^=]*?colspan='[1-9][0-9]*' rowspan='[1-9][0-9]*'", time_html):
+            t = re.search("<center>.*?</center>", ti).group(0)[8:-9].split(':')
+            for block in range(int(re.search("rowspan='[1-9][0-9]*'", ti).group(0)[9:-1])):
 
                 room_data[name]['times'].append(
-                    str(int(t[0]) + block//2 + (int(t[1])//30 + block%2) // 2) + ':' + '{0:02}'.format(
+                    str(int(t[0]) + block//2 + (int(t[1])//30 + block % 2) // 2) + ':' + '{0:02}'.format(
                         ((int(t[1])//30 + block) % 2) * 30))
 
-    # print room_data
+    with open('room_data.json', 'w') as f:
+        json.dump(room_data, f)
 
+
+def display(room_data, current_block_time):
+
+    room_data.pop('collect')
     avail_rooms = []
     for name, data in room_data.iteritems():
         avail_times = 0
         timer = current_block_time[:]
         while timer[0] + ':' + timer[1] not in data['times']:
-            if timer[0] == '25':
+            if timer[0] == '30':
                 avail_times = -1
                 break
             avail_times += 1
@@ -147,10 +146,84 @@ def collect():
         if r[1] == -1:
             t = 'None'
         else:
-            t = str(int(current_block_time[0]) + r[1]//2 + (int(current_block_time[1])//30 + r[1]%2) // 2) + \
+            t = str(int(current_block_time[0]) + r[1]//2 + (int(current_block_time[1])//30 + r[1] % 2) // 2) + \
                 ':' + \
                 '{0:02}'.format(((int(current_block_time[1])//30 + r[1]) % 2) * 30)
         print '%-9s|%13s' % (r[0], t)
 
+
+def choose_time(time):
+    if time == '':
+        current_time = datetime.datetime.now().strftime('%H:%M').split(':')
+    elif len(time) <= 2:
+        try:
+            current_time = [str(int(time)), '00']
+        except:
+            print 'Invalid Input'
+            exit()
+    elif len(time) == 3:
+        if ':' in time:
+            print 'Invalid Input'
+            exit()
+        try:
+            current_time = [str(int(time[0])), '{0:02}'.format(int(time[1:3]))]
+        except:
+            print 'Invalid Input'
+            exit()
+
+    elif len(time) == 4:
+        if ':' in time:
+            try:
+                current_time = [str(int(time[0])), '{0:02}'.format(int(time[1:3]))]
+            except:
+                print 'Invalid Input'
+                exit()
+        else:
+            try:
+                current_time = [str(int(time[0:2])), '{0:02}'.format(int(time[2:4]))]
+            except:
+                print 'Invalid Input'
+                exit()
+    elif len(time) == 5:
+        if ':' in time:
+            try:
+                current_time = [str(int(time[0:2])), '{0:02}'.format(int(time[3:5]))]
+            except:
+                print 'Invalid Input'
+                exit()
+        else:
+            print 'Invalid Input'
+            exit()
+
+    else:
+        print 'Invalid Input'
+        exit()
+
+    current_block_time = [current_time[0], '{0:02}'.format(int(current_time[1])//30 * 30)]
+
+    print('Time: %s:%s\nBlock Time: %s:%s\n' % (current_time[0], current_time[1],
+                                                current_block_time[0], current_block_time[1]))
+    return current_block_time
+
 if __name__ == '__main__':
-    collect()
+
+    with open('room_data.json') as g:
+        data = json.load(g)
+
+    # data['collect'][0] = (datetime.datetime.strptime(data['collect'][0], '%b %d, %Y') - datetime.timedelta(hours = 1)).strftime('%b %d, %Y')
+
+    print 'JSON stored date: %s' % data['collect'][0]
+
+    jsondate = datetime.datetime.strptime(data['collect'][0], '%b %d, %Y')
+    if (jsondate + datetime.timedelta(days = 1)).date() <= datetime.datetime.now().date():
+        print 'The data you have is outdated.\nUpdating data ...',
+        collect()
+        with open('room_data.json') as g:
+            data = json.load(g)
+        print 'done!'
+        print 'New JSON stored date: %s' % data['collect'][0]
+
+    custom_time = ''
+
+    custom_time = choose_time(custom_time)
+    display(data, custom_time)
